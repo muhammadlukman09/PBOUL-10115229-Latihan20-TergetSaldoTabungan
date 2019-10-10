e6fa579d7387393641d023d64e90bb22763c00a1
fakhri ramdani <muhammadlukman09@gmail.com>
2019-10-10 21:10:19 +0700

    Project Keduapuluh

---
+++ src/pboul/pkg10115229/latihan20/targetsaldotabungan/PBOUL10115229Latihan20TargetSaldoTabungan.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan20.targetsaldotabungan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lukman
+ * NIM      : 10115229
+ * NAMA     : MUHAMMAD LUKMAN HAKIM
+ * KELAS    : PBO ULANG
+ */
+public class PBOUL10115229Latihan20TargetSaldoTabungan {
+
+    static int saldoAkhir;
+    static void hitung(int a,int b){
+        saldoAkhir = (a+(a*b/100));
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    }
+    
+}
+
+ 
+ 
